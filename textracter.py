b6d1063118ab0f64438d73824ea7c642e9f38af0
grottixwoods <oikizak123@gmail.com>
2023-04-20 22:37:40 +0300

    first commit

---
+++ textracter.py
@@ -0,0 +1,22 @@
+import os
+import textract
+
+# Исходная директория
+input_directory = "D:/Projects/tsiars_gpt/runtime/docx_files/"
+
+# Конечная директория
+output_directory = "D:/Projects/tsiars_gpt/runtime/database_txt/"
+
+# список обрабатываемых textract'ом типов документов
+file_types = (".doc", ".docx", ".rtf", ".pdf", ".xlsx", ".ppt")
+
+# Проходимся по директории с условием окончания документов на file_types
+for filename in os.listdir(input_directory):
+    if filename.endswith(file_types):
+        # достаем данные из файлаов
+        text = textract.process(os.path.join(input_directory, filename)).decode("utf-8")
+        # создаем новое имя с раширением txt для файла 
+        new_filename = os.path.splitext(filename)[0] + ".txt"
+        # сохраняем файл
+        with open(os.path.join(output_directory, new_filename), "w", encoding="utf-8") as f:
+            f.write(text)
