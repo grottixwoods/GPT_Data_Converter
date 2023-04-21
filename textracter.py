5e75a96fea0ff1f0ed19a26c594e7e2d7f7bed70
grottixwoods <oikizak123@gmail.com>
2023-04-21 16:33:00 +0300

    .doc to .docx /wout anttiword

--- textracter.py
+++ textracter.py
@@ -1,14 +1,25 @@
 import os
 import textract
+import aspose.words as aw
 
 # Исходная директория
-input_directory = "./docx_files/"
+input_directory = "D:/Projects/tsiars_gpt/docx_files"
 
 # Конечная директория
-output_directory = "./database_txt/"
+output_directory = "D:/Projects/tsiars_gpt/database_txt"
 
 # список обрабатываемых textract'ом типов документов
-file_types = (".doc", ".docx", ".rtf", ".pdf", ".xlsx", ".ppt")
+file_types = (".docx", ".pdf", ".xlsx", ".ppt")
+
+#пересохраняем .doc в .docx
+for filename in os.listdir(input_directory):
+    if filename.endswith(".doc"):
+        input_path = os.path.join(input_directory, filename)
+        output_path = os.path.join(input_directory, filename.split(".")[0] + ".docx")
+        doc = aw.Document(input_path)
+        doc.save(output_path)
+        os.remove(input_path)
+
 
 # Проходимся по директории с условием окончания документов на file_types
 for filename in os.listdir(input_directory):
@@ -20,3 +31,12 @@ for filename in os.listdir(input_directory):
         # сохраняем файл
         with open(os.path.join(output_directory, new_filename), "w", encoding="utf-8") as f:
             f.write(text)
+
+for filename in os.listdir(output_directory):
+    if filename.endswith('.txt'):
+        filepath = os.path.join(output_directory, filename)
+        with open(filepath, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+        lines = [line for line in lines if line.strip()] 
+        with open(filepath, 'w', encoding='utf-8') as f:
+            f.writelines(lines)
