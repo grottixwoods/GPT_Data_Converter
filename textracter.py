df639cd1f98bc086db66e86ccde6e880ae123ffb
Tony <savson143@gmail.com>
2023-04-22 15:16:50 +0300

    feature

--- textracter.py
+++ textracter.py
@@ -3,40 +3,49 @@ import textract
 import aspose.words as aw
 
 # Исходная директория
-input_directory = "D:/Projects/tsiars_gpt/docx_files"
+input_directory = 'D:/papka/'
 
 # Конечная директория
-output_directory = "D:/Projects/tsiars_gpt/database_txt"
+output_directory = 'D:/papka/tsiars_gpt/database_txt'
 
 # Список обрабатываемых textract'ом типов документов
-file_types = (".docx", ".pdf", ".xlsx", ".ppt")
+file_types = ('.docx', '.pdf', '.xlsx', '.ppt', '.xls')
 
-# Пересохраняем .doc в .docx (Только для Win версии)
-for filename in os.listdir(input_directory):
-    if filename.endswith(".doc"):
-        input_path = os.path.join(input_directory, filename)
-        output_path = os.path.join(input_directory, filename.split(".")[0] + ".docx")
-        doc = aw.Document(input_path)
-        doc.save(output_path)
-        os.remove(input_path)
 
-# Проходимся по директории с условием окончания документов на file_types (P.S. Antiword работает только на Linux)
-for filename in os.listdir(input_directory):
-    if filename.endswith(file_types):
-        # Достаем данные из файлов
-        text = textract.process(os.path.join(input_directory, filename)).decode("utf-8")
-        # Создаем новое имя с расширением txt для файла
-        new_filename = os.path.splitext(filename)[0] + ".txt"
-        # Сохраняем файл
-        with open(os.path.join(output_directory, new_filename), "w", encoding="utf-8") as f:
-            f.write(text)
-# Проходимся по директории с условием окончания документов на .txt
-for filename in os.listdir(output_directory):
-    if filename.endswith('.txt'):
-        filepath = os.path.join(output_directory, filename)
-        with open(filepath, 'r', encoding='utf-8') as f:
-            lines = f.readlines()
-        # Убираем пустые cтроки
-        lines = [line for line in lines if line.strip()] 
-        with open(filepath, 'w', encoding='utf-8') as f:
-            f.writelines(lines)
+def converter():
+    # Пересохраняем .doc в .docx (Только для Win версии)
+    for filename in os.listdir(input_directory):
+        if filename.endswith('.doc'):
+            input_path = os.path.join(input_directory, filename)
+            output_path = os.path.join(input_directory,
+                                       filename.split('.')[0] + '.docx')
+            doc = aw.Document(input_path)
+            doc.save(output_path)
+            os.remove(input_path)
+
+    # Проходимся по директории с условием окончания документов на file_types
+    # (P.S. Antiword работает только на Linux)
+    for filename in os.listdir(input_directory):
+        if filename.endswith(file_types):
+            # Достаем данные из файлов
+            input_path = os.path.join(input_directory, filename)
+            text = textract.process(os.path.join(input_directory,
+                                                 filename)).decode('utf-8')
+            # Создаем новое имя с расширением txt для файла
+            new_filename = os.path.splitext(filename)[0] + '.txt'
+            # Сохраняем файл
+            with open(os.path.join(output_directory, new_filename), 'w',
+                      encoding='utf-8') as f:
+                f.write(text)
+            os.remove(input_path)
+    # Проходимся по директории с условием окончания документов на .txt
+    for filename in os.listdir(output_directory):
+        if filename.endswith('.txt'):
+            input_path = os.path.join(input_directory, filename)
+            filepath = os.path.join(output_directory, filename)
+            with open(filepath, 'r', encoding='utf-8') as f:
+                lines = f.readlines()
+            # Убираем пустые cтроки
+            lines = [line for line in lines if line.strip()]
+            with open(filepath, 'w', encoding='utf-8') as f:
+                f.writelines(lines)
