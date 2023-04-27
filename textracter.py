1ea86f43554e163e5dd10f452c4cd49fdff99174
Tony <savson143@gmail.com>
2023-04-27 18:34:37 +0300

    опа

--- textracter.py
+++ textracter.py
@@ -10,6 +10,7 @@ output_txt = 'output_txt'
 # Список обрабатываемых textract'ом типов документов
 file_types = ('.docx', '.pdf', '.xlsx', '.ppt', '.xls')
 
+
 def converter():
     # Пересохраняем .doc в .docx (Только для Win версии)
     if platform.system() == "Windows":
