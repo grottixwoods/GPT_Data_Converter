895a3df1f8f0e3de999adef3bba99e5fc947f5f4
Sergey Kopytko <fib3301@gmail.com>
2023-05-03 17:23:08 +0300

    Добавление условия "если pdf скан"

--- pdf2img2txt.py
+++ pdf2img2txt.py
@@ -1,20 +1,28 @@
+import PyPDF2
 import pytesseract
 from pdf2image import convert_from_path
 import glob
 
-import os
+
+def has_text(pdf_file_path):
+    with open(pdf_file_path, 'rb') as f:
+        pdf_reader = PyPDF2.PdfReader(f)
+        for page in pdf_reader.pages:
+            text = page.extract_text()
+            if text:
+                return True
+    return False
+
 
 pdfs = glob.glob(r"input_files/*.pdf")
-i = 0
 for pdf in pdfs:
-    pages = convert_from_path(pdf, 400)
-    print(i, 'картинки')
-    i += 1
-    text = ""
-    for pageNum, imgBlob in enumerate(pages):
-        print(i, 'Текстики')
-        i += 1
-        text += pytesseract.image_to_string(imgBlob, lang='rus')+'\n'
-    with open(f'{pdf[:-4]}.txt', 'w') as the_file:
-        the_file.write(text)
-        os.remove(pdf)
+    if has_text(pdf):
+        print(f'PDF-файл {pdf} содержит текст')
+    else:
+        pages = convert_from_path(pdf, 500)
+        text = ""
+        for pageNum, imgBlob in enumerate(pages):
+            text += pytesseract.image_to_string(imgBlob, lang='rus') + '\n'
+        with open(f'{pdf[:-4]}.txt', 'w') as the_file:
+            the_file.write(text)
+        print(f'PDF-файл {pdf} не содержит текст')
