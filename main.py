285ef92616b4aa963ba36a6d69004dbe6ea70b14
grottixwoods <oikizak123@gmail.com>
2023-05-04 22:28:58 +0300

    Updated textracter.py and main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from textracter import converter
+from textracter import convert_doc_to_docx, metadata_extracter, textract_converter, lines_editor
+
+# Исходная директория
+input_files = 'input_files'
+# Конечная директория
+output_txt = 'output_txt'
 
 if __name__ == '__main__':
-    converter()
+    convert_doc_to_docx(input_files)
+    metadata_extracter(input_files, output_txt)
+    textract_converter(input_files, output_txt)
+    lines_editor(output_txt)
