009551f67dfe4b9f96d6939220771cdf4682a119
grottixwoods <oikizak123@gmail.com>
2023-05-04 22:28:58 +0300

    Updated textracter.py and main.py

--- textracter.py
+++ textracter.py
@@ -1,99 +1,104 @@
 import os
 import textract
-import platform
 import csv
-from docx import Document
 import PyPDF2
+import pytesseract
+import win32com.client as win32
+import re
 
-# Исходная директория
-input_files = 'input_files'
-# Конечная директория
-output_txt = 'output_txt'
+from pdf2image import convert_from_path
+from pdf2img2txt import has_text
+from win32com.client import constants
+from docx import Document
 
 # Список обрабатываемых textract'ом типов документов
 file_types = ('.docx', '.pdf', '.xlsx', '.ppt', '.xls')
 
-def converter():
-    # Пересохраняем .doc в .docx (Только для Win версии)
-    if platform.system() == "Windows":
-        import aspose.words as aw
-        for filename in os.listdir(input_files):
-            if filename.endswith('.doc'):
-                input_path = os.path.join(input_files, filename)
-                output_path = os.path.join(input_files,
-                                           filename.split('.')[0] + '.docx')
-                doc = aw.Document(input_path)
-                doc.save(output_path)
-                os.remove(input_path)
+def convert_doc_to_docx(input_files):
+       
+    for filename in os.listdir(input_files):
+        # Opening MS Word
+        input_path = os.path.join('D:\\Projects\\tsiars_gpt\\input_files', filename)
+        word = win32.gencache.EnsureDispatch('Word.Application')
+        doc = word.Documents.Open(input_path)
+        print(doc)
+        doc.Activate()
 
+        # Rename path with .docx
+        new_file_abs = os.path.abspath(input_path)
+        new_file_abs = re.sub(r'\.\w+$', '.docx', new_file_abs)
+
+        # Save and Close
+        word.ActiveDocument.SaveAs(
+            new_file_abs, FileFormat=constants.wdFormatXMLDocument
+        )
+        doc.Close(False)
+        os.remove(input_path)
+
+
+def metadata_extracter(input_files, output_txt):
+    
     csv_file = open(os.path.join(output_txt, "metadata.csv"), 'w', newline='')
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow(["Metadata", "Path to file"])
 
-    # Проходимся по директории с условием окончания документов на file_types
-    # (P.S. Antiword работает только на Linux)
     for filename in os.listdir(input_files):
         if filename.endswith(file_types):
-            # Достаем данные из файлов
-            input_path = os.path.join(input_files, filename)
             if filename.endswith(".docx"):
-                # Extract metadata from a Word document
+                # Word Meta
                 doc = Document(os.path.join(input_files, filename))
                 metadata = doc.core_properties
                 metadata_dict = {"Title": metadata.title,
-                                 "Author": metadata.author,
-                                 "Subject": metadata.subject,
-                                 "Keywords": metadata.keywords,
-                                 "Category": metadata.category,
-                                 "Comments": metadata.comments}
-                # Write the metadata to the CSV file
-                csv_writer.writerow
-                (
-                    [
-                        metadata_dict, os.path.join(
-                                                 output_txt,
-                                                 f'{filename[:-5]}.txt'
-                                                )
-                    ]
-                )
+                                "Author": metadata.author,
+                                "Subject": metadata.subject,
+                                "Keywords": metadata.keywords,
+                                "Category": metadata.category,
+                                "Comments": metadata.comments}
+                csv_writer.writerow([metadata_dict, os.path.join(output_txt, f'{filename[:-5]}.txt')])
+
             elif filename.endswith(".pdf"):
-                # Extract metadata from a PDF document
+                # Pdf Meta
                 pdf_file = open(os.path.join(input_files, filename), 'rb')
-                pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-                metadata_dict = pdf_reader.getDocumentInfo()
-                # Write the metadata to the CSV file
-                csv_writer.writerow
-                (
-                    [
-                        metadata_dict, os.path.join(
-                                                 output_txt,
-                                                 f'{filename[:-5]}.txt'
-                                                )
-                    ]
-                )
-            text = textract.process(os.path.join(input_files,
-                                                 filename)).decode('utf-8')
-            # Создаем новое имя с расширением txt для файла
-            new_filename = os.path.splitext(filename)[0] + '.txt'
-            # Сохраняем файл
-            with open(os.path.join(output_txt, new_filename), 'w',
-                      encoding='utf-8') as f:
-                f.write(text)
+                pdf_reader = PyPDF2.PdfReader(pdf_file)
+                metadata_dict = pdf_reader.metadata
+                csv_writer.writerow([metadata_dict, os.path.join(output_txt, f'{filename[:-5]}.txt')])
     csv_file.close()
 
-    # Проходимся по директории с условием окончания документов на .txt
+def textract_converter(input_files, output_txt):
+    
+    csv_file = open(os.path.join(output_txt, "metadata.csv"), 'w', newline='')
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(["Metadata", "Path to file"])
+    
+    for filename in os.listdir(input_files):
+
+        # if filename.endswith('.pdf'):
+        #     if has_text(filename):
+        #         print(f'PDF-файл {filename} содержит текст')
+        #     else:
+        #         print(f'PDF-файл {filename} не содержит текст')
+        #         pages = convert_from_path(f'input_files/{filename}', 500)
+        #         text = ""
+        #         print(f'PDF-файл {filename} не содержит текст2')
+        #         for pageNum, imgBlob in enumerate(pages):
+        #             text += pytesseract.image_to_string(imgBlob, lang='rus') + '\n'
+        #             print(f'PDF-файл {filename} не содержит текст3')
+        #         with open(f'{filename[:-4]}.txt', 'w') as the_file:
+        #             the_file.write(text)
+
+        if filename.endswith(file_types):
+            text = textract.process(os.path.join(input_files, filename)).decode('utf-8')
+            new_filename = os.path.splitext(filename)[0] + '.txt'
+            with open(os.path.join(output_txt, new_filename), 'w', encoding='utf-8') as f:
+                f.write(text)
+
+def lines_editor(output_txt):
+
     for filename in os.listdir(output_txt):
         if filename.endswith('.txt'):
             filepath = os.path.join(output_txt, filename)
             with open(filepath, 'r', encoding='utf-8') as f:
                 lines = f.readlines()
             lines = [line for line in lines if line.strip()]
-            # Убираем вотермарки
-            if 'Created with an evaluation copy of Aspose.Words.' in lines[-1]:
-                lines.pop()
-                lines.pop(0)
-            # Убираем пустые cтроки
             with open(filepath, 'w', encoding='utf-8') as f:
                 f.writelines(lines)
-
-    os.remove(input_path)
